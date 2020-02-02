12b06777e2e08347235c70e729a72f29405778d4
Ramyasara <ramyabanu1815@gmail.com>
2020-02-02 11:23:14 +0530

    new code

---
+++ Java Project/src/Org/test/Employee.java
@@ -0,0 +1,18 @@
+package Org.test;
+
+public class Employee {
+	private void Java() {
+		
+System.out.println("Welcome to Java");
+	}
+	private void Oracle() {
+		System.out.println("Welcome to Oracle");
+
+	}
+	public static void main(String[] args) {
+		Employee e =  new Employee();
+		e.Java();
+		e.Oracle();
+	}
+
+}
